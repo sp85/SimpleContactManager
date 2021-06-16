4b7ab9b836aaf9efe34fb412ed8f0579c0611e4f
sabil patel <sabilpatel85@hotmail.com>
2021-06-16 14:32:00 -0400

    Started project. Added db context, contacts and email address classes.

---
+++ SimpleContactManager/Data/ContactContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SimpleContactManager.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace SimpleContactManager.Data
+{
+    public class ContactContext:DbContext
+    {
+    }
+}
---
+++ SimpleContactManager/Models/EmailAddress.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleContactManager.Models
+{
+    public class EmailAddress
+    {
+        public bType? bType { get; set; }
+
+        public string email { get; set; }
+
+    }
+
+    public enum bType
+    {
+        personal,
+        business
+    }
+}
